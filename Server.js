ca36270b776d98f3cffd3e6d7a15f26a3d1e7cec
rdex999 <didabom2150@gmail.com>
2023-08-08 18:02:51 +0300

    Added some comments on Server.js

--- Server.js
+++ Server.js
@@ -1,17 +1,22 @@
 const Express = require("express");
 const Server = Express();
 
+// Set view engine
 Server.set("view engine", "ejs");
 
+// Listen on port 8080
 Server.listen(8080, () => {
     console.log("\nStarting server on port 8080.\n");
 });
 
+// Make everything in the "Public" folder available from the browser (a stylesheet.css file for example)
 Server.use(Express.static("Public"));
 
+// Make json available (from post requests and stf)
 Server.use(Express.json());
 Server.use(Express.urlencoded({ extended: true }));
 
+// Prints some information about every request to the server
 Server.use((req, res, next) => {
     console.log(`\nRequest from: ${req.ip}\nType: ${req.method}\nPath: ${req.path}\nTime: ${Date()}`);
     next();
