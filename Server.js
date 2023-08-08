59a56031cb8ec79d3a3c020af14f959756aa3af2
rdex999 <didabom2150@gmail.com>
2023-08-08 14:28:26 +0300

    Added more css. Made the sign up page send a post request with the username&password. Made the signup page respond with the username (for testing)

--- Server.js
+++ Server.js
@@ -1,14 +1,20 @@
+const { time } = require("console");
 const Express = require("express");
 const Server = Express();
 
 Server.set("view engine", "ejs");
 
-Server.listen(8080);
+Server.listen(8080, () => {
+    console.log("\nStarting server on port 8080.\n");
+});
 
 Server.use(Express.static("Public"));
 
+Server.use(Express.json());
+Server.use(Express.urlencoded({ extended: true }));
+
 Server.use((req, res, next) => {
-    console.log(`Request from: ${req.hostname}\nType: ${req.method}\nPath: ${req.path}`);
+    console.log(`\nRequest from: ${req.ip}\nType: ${req.method}\nPath: ${req.path}\nTime: ${Date()}`);
     next();
 });
 
@@ -17,10 +23,13 @@ Server.get("/", (req, res) => {
 });
 
 Server.get("/SignUp", (req, res) => {
-    res.render("SignUp");
+    res.render("SignUp", {SignUpState: false});
+});
+
+Server.post("/SignUp", (req, res) => {
+    res.render("SignUp", {username: req.body.username, SignUpState: true});
 });
 
 Server.use((req, res) => {
-    res.status = 404; 
-    res.render("404");
+    res.status = 404; res.render("404");
 });
