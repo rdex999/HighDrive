695cb4a68fadf456d3bb02198c65bb4b269db4b3
rdex999 <didabom2150@gmail.com>
2023-08-16 12:47:39 +0300

    Finally done styling the top bar

--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -6,6 +6,8 @@ const Layout = () => {
         <div>
             <div className="container-fluid p-4 my-4 border list-inline">
                 <Link to="/" className="link-offset-3-hover link-underline-opacity-0 link-underline-opacity-75-hover fs-1 link-dark">Home Drive</Link> 
+                <Link to="login" className="link-offset-3-hover link-underline-opacity-0 link-underline-opacity-75-hover fs-1 link-dark float-end">Log in</Link>
+                <Link to="/signup" className="link-offset-3-hover link-underline-opacity-0 link-underline-opacity-75-hover fs-1 link-dark float-end me-4">Sign up</Link>
             </div>
             <Outlet/>
         </div>
